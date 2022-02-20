0175f5aeeb91b734d9d991a2e388e2c1794a212c
UsernameForTheWin <97945494+UsernameForTheWin@users.noreply.github.com>
2022-02-20 19:49:38 +0530

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8
 export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
 export ARCH=arm
 
