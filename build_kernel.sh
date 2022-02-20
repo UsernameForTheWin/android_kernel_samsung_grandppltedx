d6e3d12c1104c348038c9bdca0c6aa8df1fc2d74
UsernameForTheWin <97945494+UsernameForTheWin@users.noreply.github.com>
2022-02-20 19:51:10 +0530

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+cd ~
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=~/arm-eabi-4.8/bin/arm-eabi-
 export ARCH=arm
 
 make mt6737t-grandpplte_defconfig
